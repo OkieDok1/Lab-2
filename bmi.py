de5b3b51c5570f60e11cbcf6420cab883c6fc451
xav <tanyxxavier.22@ichat.sp.edu.sg>
2023-10-31 10:21:57 +0800

    adjusted bmi result value

--- bmi.py
+++ bmi.py
@@ -7,11 +7,11 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
 
     if bmi < 18.5:
-        print("-1")
+        print("Underweight")
     elif bmi >= 18.5 and bmi <= 20.5:
-        print("0")
+        print("Normal weight")
     else:
-        print("1")
+        print("Overweight")
 
 
 calculate_bmi(weight=57, height=1.73)
